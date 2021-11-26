7a58689d0d569e772f57d69875d8f86d0fa9836c
Raphaël THIBAUT <thibaut.raphael@protonmail.com>
2021-11-26 23:39:54 +0100

    Avancement Models

---
+++ Shopping API/models/Users.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const {
+    boolean
+} = require('webidl-conversions');
+
+const UsersSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true // Cannot replicate this user name
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }, //
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("User", UsersSchema);
