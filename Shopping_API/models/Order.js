348f4ed590dc5ff7ccf672e51f39332c85260060
Raphaël THIBAUT <thibaut.raphael@protonmail.com>
2021-11-30 23:10:23 +0100

    Correction du model Order

--- Shopping_API/models/Order.js
+++ Shopping_API/models/Order.js
@@ -1,26 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const OrderSchema = new mongoose.Schema({
-    userID: {
-        type: String,
-        required: true
-    },
-    description: [
-        {
-            productID: {
-                type: String
-            },
-            quantity: {
-                type: Number,
-                default: 1
-            }
-        }
+const OrderSchema = new mongoose.Schema(
+  {
+    userID: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
     ],
     amount: { type: Number, required: true },
-    address: { type: Object, required: true}, // L'objet contiendra toutes les infos d'une adresse Rue, Ville, Pays ...
-    status: { type: String, default: 'pending'}
-}, {
-    timestamps: true
-});
+    address: { type: Object, required: true },
+    status: { type: String, default: "pending" },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Order", OrderSchema);
