56866aafda90819cb9e342b7b8eda2d283a86998
Raphaël THIBAUT <thibaut.raphael@protonmail.com>
2021-11-30 23:09:37 +0100

    Ajout des routes cart & order

--- Shopping_API/index.js
+++ Shopping_API/index.js
@@ -5,9 +5,12 @@ const dotenv = require("dotenv");
 const userRoots = require("./routes/users");
 const authRoots = require("./routes/auth");
 const productRoots = require("./routes/product");
+const cartRoots = require("./routes/cart");
+const orderRoots = require("./routes/order");
 
 dotenv.config(); //Key : Value de ce que contient le fichier .env
 
+//TODO: En cas de mauvaise requete HTTP faire en sorte que l'API ne crash pas
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Success"))
@@ -17,6 +20,8 @@ app.use(express.json()); //Permet d'utiliser le JSON
 app.use("/api/user", userRoots);
 app.use("/api/auth", authRoots); //Route vers le module d'authentification
 app.use("/api/product", productRoots);
+app.use("/api/cart", cartRoots);
+app.use("/api/order", orderRoots);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend Server is running");
